937a0754426fca0b8869159217609249511ad0ca
theketan26 <ketangsolanki129@gmail.com>
2024-01-24 10:59:25 +0530

    Client and server seperation

---
+++ src/actions/authActions.js
@@ -0,0 +1,6 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const login = createAction('login');
+export const logout = createAction('logout');
+export const setPosition0 = createAction('setPosition0');
+export const setPosition1 = createAction('setPosition1');
